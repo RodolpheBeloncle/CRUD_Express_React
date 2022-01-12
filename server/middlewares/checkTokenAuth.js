d92cb66d16fb25fa340e2813ad6eed1571acc1af
Rodolphe Beloncle <69888160+RodolpheBeloncle@users.noreply.github.com>
2022-01-12 15:47:26 +0100

    Update checkTokenAuth.js

--- server/middlewares/checkTokenAuth.js
+++ server/middlewares/checkTokenAuth.js
@@ -2,21 +2,29 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
-const checkJwt = (req, res, next) => {
-  // Si il n'a pas de header Authorization
-  if (!req.headers.authorization) {
-    res.status(401).json();
+
+
+/* Vérification du token */
+const isAuthorized = (req, res, next) => {
+  // Récupération du token
+  const token = req.headers.authorization 
+
+  // Présence d'un token
+  if (!token) {
+      return res.status(401).json({ message: 'Error. Need a token' })
   }
 
-  // si l'utilisateur est bien connecté avec un JWT valide, je continue
-  try {
-    jwt.verify(req.headers.authorization, process.env.SECRET_TOKEN);
-    console.log(req.headers.authorization)
-    return next();
-  } catch (err) {
-    // sinon on lui renvoi une erreur
-    return res.status(401).json();
-  }
-};
+  // Véracité du token
+  jwt.verify(token,  process.env.SECRET_TOKEN, (err, decodedToken) => {
+    console.log("decodedtoken",decodedToken)
+      if (err) {
+          res.status(401).json({ message: 'Error. Bad token' })
+      } else {
+        console.log("decodedToken_id",decodedToken.id)
+        res.locals = decodedToken.id;
+        return next()
+      }
+  })
+}
 
-module.exports = checkJwt;
+module.exports = isAuthorized;
